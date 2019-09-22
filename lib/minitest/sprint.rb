5c348335a0c22dba973ac3cc3221699813b3976d
Ryan Davis <ryand@zenspider.com>
2019-09-22 14:14:32 -0800

    + Refactored and moved bin/minitest to Minitest::Sprint.run. [git-p4: depot-paths = "//src/minitest-sprint/dev/": change = 12263]

--- lib/minitest/sprint.rb
+++ lib/minitest/sprint.rb
@@ -1,3 +1,15 @@
+require "minitest/autorun"
+require "minitest/path_expander"
+
+$LOAD_PATH.unshift "test"
+$LOAD_PATH.unshift "lib"
+
 class Minitest::Sprint
   VERSION = "1.2.0"
+
+  def self.run args = ARGV
+    Minitest::PathExpander.new(args).process.each do |f|
+      require "./#{f}"
+    end
+  end
 end
