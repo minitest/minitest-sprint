6ec83c29c50564926df3ab489dca53ece5957cc9
Ryan Davis <ryand@zenspider.com>
2019-09-22 16:16:17 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest-sprint/dev/": change = 12276]

--- lib/minitest/sprint.rb
+++ lib/minitest/sprint.rb
@@ -5,7 +5,7 @@ $LOAD_PATH.unshift "test"
 $LOAD_PATH.unshift "lib"
 
 class Minitest::Sprint
-  VERSION = "1.2.0"
+  VERSION = "1.2.1"
 
   def self.run args = ARGV
     Minitest::PathExpander.new(args).process.each do |f|
