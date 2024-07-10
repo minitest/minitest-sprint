8cccf39da19987b10c75328466589606e7a8bb9c
Adam Daniels <adam@mediadrive.ca>
2024-07-10 14:50:07 -0400

    Display rake task name in result list

--- lib/minitest/rake_reporter.rb
+++ lib/minitest/rake_reporter.rb
@@ -4,7 +4,7 @@ module Minitest
   class RakeReporter < SprintReporter
     def print_list
       results.each do |result|
-        puts "  rake N=#{result.class_name}##{result.name}"
+        puts ["  rake", options[:rake_task], "N=#{result.class_name}##{result.name}"].compact.join(" ")
       end
     end
   end
