50f3bb487095ed38371e3a593d8a4e95af9b4441
Ryan Davis <ryand-ruby@zenspider.com>
2024-07-23 10:31:02 -0800

    Branching minitest-sprint to version 1.3.0 [git-p4: depot-paths = "//src/minitest-sprint/dev/": change = 14259]

--- lib/minitest/sprint.rb
+++ lib/minitest/sprint.rb
@@ -5,7 +5,7 @@ $LOAD_PATH.unshift "test"
 $LOAD_PATH.unshift "lib"
 
 class Minitest::Sprint
-  VERSION = "1.2.2"
+  VERSION = "1.3.0"
 
   def self.run args = ARGV
     Minitest::PathExpander.new(args).process.each do |f|
