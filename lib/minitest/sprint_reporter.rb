8bd6c8e95067b21fb408e8418e5282c0e68f13c5
Ryan Davis <ryand-ruby@zenspider.com>
2025-12-02 13:12:11 -0800

    + Folded BinstubReporter into SprintReporter and added line number reporting.
    [git-p4: depot-paths = "//src/minitest-sprint/dev/": change = 14636]

--- lib/minitest/sprint_reporter.rb
+++ lib/minitest/sprint_reporter.rb
@@ -1,9 +1,11 @@
 module Minitest
   class SprintReporter < AbstractReporter
+    attr_accessor :style
     attr_accessor :results
 
-    def initialize
+    def initialize style = :regexp
       self.results = []
+      self.style = style
     end
 
     def record result
@@ -18,6 +20,28 @@ module Minitest
       puts
       print_list
       puts
+    end
+
+    def print_list
+      case style
+      when :regexp
+        results.each do |result|
+          puts "  minitest -n #{result.class_name}##{result.name}"
+        end
+      when :lines
+        files = Hash.new { |h,k| h[k] = [] }
+        results.each do |result|
+          path, line = result.source_location
+          path = path.delete_prefix "#{Dir.pwd}/"
+          files[path] << line
+        end
+
+        files.sort.each do |path, lines|
+          puts "  minitest %s:%s" % [path, lines.sort.join(",")]
+        end
+      else
+        raise "unsupported style: %p" % [style]
+      end
     end
   end
 end
